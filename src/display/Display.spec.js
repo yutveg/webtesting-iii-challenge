9a826d2b2880ded12cd38fef2aa9eab223f13ae1
yutveg <kodi.0n31l@gmail.com>
2019-12-16 13:14:58 -0800

    75% done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+
+test("Display toggles unlocked/locked and closed/open accordingly", () => {
+  const { getByText, rerender } = render(<Display locked closed />);
+  getByText(/locked/i);
+  getByText(/closed/i);
+  rerender(<Display locked={false} closed={false} />);
+  getByText(/unlocked/i);
+  getByText(/open/i);
+});
