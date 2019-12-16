05ab2d43c2f99640168a0e275a69a558e5e9961d
yutveg <kodi.0n31l@gmail.com>
2019-12-16 13:08:48 -0800

    50% of testing done

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Controls = props => {
   const { locked, closed, toggleLocked, toggleClosed } = props;
 
   return (
-    <div className="controls panel">
+    <div className="controls panel" data-testid="controls">
       <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
-        {locked ? 'Unlock Gate' : 'Lock Gate'}
+        {locked ? "Unlock Gate" : "Lock Gate"}
       </button>
       <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
-        {closed ? 'Open Gate' : 'Close Gate'}
+        {closed ? "Open Gate" : "Close Gate"}
       </button>
     </div>
   );
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
-// Test away
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+test("Dashboard is rendering control and display components", () => {
+  const { getByTestId } = render(<Dashboard />);
+  getByTestId("display");
+  getByTestId("controls");
+});
