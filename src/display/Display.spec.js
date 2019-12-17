e5edfa6a40f0f79340541124683d74e9b199a893
yutveg <kodi.0n31l@gmail.com>
2019-12-16 16:55:33 -0800

    class tests now functional

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Display from "./Display";
+import { toHaveClass } from "@testing-library/jest-dom";
+
+expect.extend({ toHaveClass });
 
 test("Display toggles unlocked/locked and closed/open accordingly", () => {
   const { getByText, rerender } = render(<Display locked closed />);
@@ -9,4 +12,13 @@ test("Display toggles unlocked/locked and closed/open accordingly", () => {
   rerender(<Display locked={false} closed={false} />);
   getByText(/unlocked/i);
   getByText(/open/i);
+});
+
+test("Displays toggle red or green depending on locked/closed status", () => {
+  const { getByText, rerender } = render(<Display locked closed />);
+  expect(getByText(/locked/i)).toHaveClass("red-led");
+  expect(getByText(/closed/i)).toHaveClass("red-led");
+  rerender(<Display locked={false} closed={false} />);
+  expect(getByText(/open/i)).toHaveClass("green-led");
+  expect(getByText(/unlocked/i)).toHaveClass("green-led");
 });
